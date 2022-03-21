f60e3019defba62e15e9bb35a04593652c0919b6
Edwin Marroquin <devem.software@gmail.com>
2022-03-20 20:54:20 -0500

    FIX: changes to vue-router to hashmode

---
+++ docs/assets/CollaboratorView.2742f606.js
@@ -0,0 +1,1 @@
+import{c as t}from"./collaborators.94738893.js";import{f as e,o as s,c as a,t as c}from"./vendor.894c581b.js";const f={props:{id:{type:String,required:!0}},setup(r){const o=e(t[r.id]);return(n,l)=>(s(),a("code",null,c(o.value),1))}};export{f as default};
--- docs/assets/CollaboratorView.c41017cb.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./collaborators.94738893.js";import{f as e,o as s,c as a,t as c}from"./vendor.9c4fe745.js";const f={props:{id:{type:String,required:!0}},setup(r){const o=e(t[r.id]);return(n,l)=>(s(),a("code",null,c(o.value),1))}};export{f as default};
