04fa18fed3ebb6e5c8beaad66b80964f835c82ec
Edwin Marroquin <devem.software@gmail.com>
2022-03-23 22:10:16 -0500

    UNESTABLE: Add PWA support

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,16 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   pwa: {
     // ...Other pwa config options
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: {
+      background_color: "#4DBA87",
+      display: "standalone",
+      orientation: "portrait",
+
+    },
     icons: {
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
@@ -9,5 +19,5 @@ module.exports = {
       maskIcon: "img/icons/safari-pinned-tab.svg",
       msTileImage: "img/icons/msapplication-icon-144x144.png",
     },
-  },
+  }
 };
